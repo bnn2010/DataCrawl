dfb942b20099d233119fb45cf63bfaff624b3f46
Administrator <bun@csip.org.cn>
2017-04-24 10:44:18 +0800

    测试

---
+++ src/test/java/com/bun/datacrawl/BaiduDataTest.java
@@ -0,0 +1,18 @@
+package com.bun.datacrawl;
+
+/**
+ * Created by bun@csip.org.cn on 2017/4/24.
+ */
+public class BaiduDataTest {
+    @org.junit.Test
+    public void getRealLinkFromBaiduLink() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void jsoupTest() throws Exception {
+        BaiduData.jsoupTest();
+
+    }
+
+}
