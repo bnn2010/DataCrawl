b243f3987f92b55175ba990cc25bafa6b8e23bb7
buning <947211263@qq.com>
2017-04-25 07:17:57 +0800

    抓取数据

--- src/main/java/com.bun.datacrawl/BaiduData.java
+++ src/main/java/com.bun.datacrawl/BaiduData.java
@@ -1,14 +1,56 @@
 package com.bun.datacrawl;
 
+import com.bun.datacrawl.util.spiderUtil.PostUtil;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by bun@csip.org.cn on 2017/4/24.
  */
 public class BaiduData {
+
+    public void getBaiduData(){
+        String query = "site:gov.cn 大气and文件and(京津冀or北京or天津or河北or山东or山西or内蒙古)";
+
+        try {
+            String urlquery = java.net.URLEncoder.encode(query,"utf-8");
+//            String pn=
+            String urlBuilder = "http://www.baidu.com/s?wd="+urlquery;
+            System.out.println("urlEncoderStr:"+urlBuilder);
+            String content = PostUtil.excuteGet(urlBuilder);
+            Document doc = Jsoup.parse(content,"utf-8");
+            Elements links=doc.select("a[href]");
+            Element first=doc.select("id.1").first();
+            System.out.println(first);
+//            for (Element link:links
+//                 ) {
+//                System.out.println(link);
+//            }
+//            Element contentLeft = doc.select("div.content_left").first();
+//            System.out.println(contentLeft);
+//            for (Element item:contentLefts
+//                 ) {
+//                System.out.println(item.html());
+//                System.out.println("--------------------");
+//            }
+//            System.out.println(doc.body());
+
+
+//            System.out.println(content);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 
 
 
