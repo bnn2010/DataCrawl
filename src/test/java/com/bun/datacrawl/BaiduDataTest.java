b597da0813c1a24de85798906c14eb345a5fcd42
buning <947211263@qq.com>
2017-04-25 07:18:14 +0800

    测试类

--- src/test/java/com/bun/datacrawl/BaiduDataTest.java
+++ src/test/java/com/bun/datacrawl/BaiduDataTest.java
@@ -1,9 +1,17 @@
 package com.bun.datacrawl;
 
+import org.junit.Test;
+
 /**
  * Created by bun@csip.org.cn on 2017/4/24.
  */
 public class BaiduDataTest {
+    @Test
+    public void getBaiduData() throws Exception {
+    BaiduData bd = new BaiduData();
+    bd.getBaiduData();
+    }
+
     @org.junit.Test
     public void getRealLinkFromBaiduLink() throws Exception {
 
